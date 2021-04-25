a6b5da154ddc24f8824a3a20d3e1b7be8a13bdce
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-04-25 13:05:16 -0400

    structure made up

---
+++ bmmt/src/main/java/com/bbmt/BmmtApplication.java
@@ -0,0 +1,13 @@
+package com.bbmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmmtApplication.class, args);
+	}
+
+}
---
+++ bmmt/src/test/java/com/bbmt/BmmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
