330d8e99e28cc96682d9c5d9661f703d88f26c47
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-04-25 15:15:50 -0400

    made service and entity for Account

--- bmmt/src/main/java/entities/Account.java
+++ bmmt/src/main/java/entities/Account.java
@@ -6,7 +6,54 @@ import javax.persistence.Id;
 
 @Entity
 public class Account {
-    @Id
-    @GeneratedValue
-    Long id;
+    private @Id @GeneratedValue Long id;
+    private String accountType;
+    private Double balance;
+    private Long accountNumber;
+    private Long userId;
+
+    public Account() {}
+
+    public Account(String accountType, Double balance, Long accountNumber, Long userId) {
+        this.accountType = accountType;
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
